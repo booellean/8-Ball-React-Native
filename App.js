81af0efd0dd611f26b5ba98f2222abb64bf3ebd1
booellean <booelle.commissions@gmail.com>
2019-03-06 20:50:32 -0800

    chore: Preloading icon for app homescreen/microphone component

--- App.js
+++ App.js
@@ -30,14 +30,13 @@ export default class App extends React.Component {
   _loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
+        require('./assets/images/icon.png'),
         require('./assets/images/robot-dev.png'),
         require('./assets/images/robot-prod.png'),
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
         'amatic-regular': require('./assets/fonts/AmaticSC-Regular.ttf'),
         'amatic-bold': require('./assets/fonts/AmaticSC-Bold.ttf'),
         'annie': require('./assets/fonts/AnnieUseYourTelescope-Regular.ttf'),
