ff7b7eff0104a53bf160d6ac9557fb0207e048fe
booellean <booelle.commissions@gmail.com>
2019-03-08 14:45:58 -0800

    chore: props no longer being passed to Microphone component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -26,7 +26,7 @@ export default class HomeScreen extends React.Component {
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
 
-          <MicrophoneAccess {...this.props}/>
+          <MicrophoneAccess/>
 
           <View style={styles.getStartedContainer}>
             {/* Delete this text and uncomment to re-enable Development mode features */}
