af436078cb5d5dad67b8c2ca00d81641cec0035e
booellean <booelle.commissions@gmail.com>
2019-03-08 21:05:42 -0800

    test: not functioning, playing around with speech-to-text libraries, having issues with support

--- components/MicrophoneAccess.js
+++ components/MicrophoneAccess.js
@@ -1,25 +1,166 @@
 import React from 'react';
-import Voice from 'react-native-voice';
+import { Permissions } from "expo";
 import {
   Image,
   TouchableOpacity,
   Text,
   View,
   StyleSheet } from 'react-native';
+  import Voice from 'react-native-voice';
 
 export class MicrophoneAccess extends React.Component {
+  state = {
+    recognized: '',
+    pitch: '',
+    error: '',
+    end: '',
+    started: '',
+    results: [],
+    partialResults: [],
+  };
+
   constructor(props){
     super(props)
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechRecognized = this.onSpeechRecognized;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged;
   }
 
-  componentDidMount(){
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
 
+  onSpeechStart = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechStart: ', e);
+    this.setState({
+      started: '√',
+    });
+  };
+
+  onSpeechRecognized = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechRecognized: ', e);
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechEnd = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechEnd: ', e);
+    this.setState({
+      end: '√',
+    });
+  };
+
+  onSpeechError = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechResults: ', e);
+    this.setState({
+      results: e.value,
+    });
+  };
+
+  onSpeechPartialResults = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechPartialResults: ', e);
+    this.setState({
+      partialResults: e.value,
+    });
+  };
+
+  onSpeechVolumeChanged = e => {
+    // eslint-disable-next-line
+    console.log('onSpeechVolumeChanged: ', e);
+    this.setState({
+      pitch: e.value,
+    });
+  };
+
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+  };
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      //eslint-disable-next-line
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+  };
+
+  async componentDidMount(){
+    const { status, expires, permissions } = await Permissions.askAsync(
+      Permissions.AUDIO_RECORDING
+    );
+    if (status !== "granted") {
+      //Permissions not granted. Don't show the start recording button because it will cause problems if it's pressed.
+      this.setState({showRecordButton: false});
+    } else {
+      this.setState({showRecordButton: true});
+    }
   }
 
   render() {
     return (
       <View style={styles.welcomContainer}>
-        <TouchableOpacity onPress={this._onStartButtonPress} style={{flex: 1}}>
+        <TouchableOpacity onPress={this.onStartButtonPress} style={{flex: 1}}>
           <Image
             source={require('../assets/images/icon.png')}
             style={styles.eightBall}
@@ -29,8 +170,9 @@ export class MicrophoneAccess extends React.Component {
     );
   }
 
-  _onStartButtonPress(){
-    console.log("MicrophoneAccess button was pressed!");
+  onStartButtonPress(){
+    Voice.start('en-US');
+    console.log("testing the button");
   }
 }
 
